cb88bdf853b5530203506f8bc26a016eb4928f6e
DESKTOP-65SAUST\Alumno <ascargortageorgy@escivi.com>
2025-01-23 15:23:01 +0100

    Monedo funcional

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,29 @@
 $(document).ready(function (){
 
-        console.log("Online jQuery");
-        
+    var $coin = $("#coin");
+
+        $coin.on("click", function (){
+            
+            var randomNum = Math.random();
+            $coin.removeClass();
+            
+            setTimeout(function (){
+                
+                if (randomNum <= 0.5) {
+                    console.log("Es Cara!");
+                    $coin.addClass("iscara");
+                } 
+                           
+                else{
+                    console.log("Es Cruz!");
+                    $coin.addClass("iscruz");
+                }
+                
+            }, 100);
+
+
+
+        });
+
 
 });
